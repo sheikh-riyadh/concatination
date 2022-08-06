014849f8ba2269a2025787ae81dbb7e24e6ced38
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 13:45:26 +0600

    Concatination

---
+++ concatination.js
@@ -0,0 +1,9 @@
+/* John’s teacher gave him two variables. Each variable contains a string. John’s teacher asked him to
+combine these two strings and print them in one line. Help John write the program. */
+
+let dreamLine1 = "I am going to be ";
+let dreamLine2 = "an awesome web developer";
+
+let fullDreamLine = dreamLine1.concat(dreamLine2);
+
+console.log("My dream line is:", fullDreamLine);
